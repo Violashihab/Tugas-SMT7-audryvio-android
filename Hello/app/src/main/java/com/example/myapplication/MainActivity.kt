91f945f22ba2929e2e4083aa13e3e81d722cf984
AudryViola <violashihab17@gmail.com>
2026-01-03 12:39:41 +0700

    Upload proyek Android ke folder Hello

---
+++ Hello/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val showToastButton = findViewById<Button>(R.id.buttonToast)
+        showToastButton?.setOnClickListener {
+            Toast.makeText(this, "Halo ini Audry!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
